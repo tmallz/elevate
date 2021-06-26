bf004388ae1d2633723ccb26c29b77c72d39b08a
Tim O'Malley <tomalley24@gmail.com>
2021-06-26 11:35:42 -0500

    react + tailwind working

---
+++ elevate/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <h1 className="text-4xl font-bold text-center text-blue-500">Hello World</h1>
+  );
+}
+
+export default App;
