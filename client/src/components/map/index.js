951116ded454f839af401357deab77bc580a8204
Tim O'Malley <tomalley24@gmail.com>
2021-07-18 22:05:53 -0500

    google maps component semi running

--- client/src/components/map/index.js
+++ client/src/components/map/index.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
+import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow} from "react-google-maps"
 
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
- 
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
-    );
-  }
+function Map(props){
+  const [selectedStation, setSelectedStation] = useState(null);
+
+  return(
+    <GoogleMap 
+      defaultZoom={10}
+      defaultCenter={{lat: 41.8781, lng:-87.6298}}
+    >
+      {/* fill this in with the positional data from the API call from train stations */}
+      {props.map((station) => (
+        <Marker key={props.stationID}  
+        position={{lat: props.lat, lng: props.lng}}
+        onClick={() => {
+          setSelectedStation(station);
+        }}
+        />
+      ))}
+
+      {selectedStation && (
+        <InfoWindow
+        position={{lat: props.lat, lng: props.lng}}
+        onCloseClick={() => {
+          setSelectedStation(null);
+        }}
+        // add the info about the stations here
+        ><div>Sation Details </div></InfoWindow>
+      )}
+    </GoogleMap>
+  )
 }
- 
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyAZx2oaWVFfDoJOeaZxiE8wqY9RxWE4Mko')
-})(MapContainer)
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default WrappedMap;
