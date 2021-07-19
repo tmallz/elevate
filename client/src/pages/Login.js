615f4836f372e8cb7783d448970b488c78221fa8
Haneen Nasereddin <haneennasereddin1@gmail.com>
2021-07-19 15:34:18 -0500

    style changes

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,48 +1,48 @@
 import React, {Component } from "react";
 import API from '../utils/API';
 import './login.css';
-
+ 
 class Login extends Component{
-    state ={
-        username: '',
-        password: ''
-    }    
-
-    submitButtonhandler = e =>{
-        e.preventDefault();
-        
-    }
-
-    
-    render(){
-        return(
-            <div className="logincard">
-                <form action="/examples/actions/confirmation.php" method="post">
-                    <h2 className="text-center">Log in</h2>       
-                    <div className="card">
-                        <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="Username" 
-                        required="required"
-                        />
-                    </div>
-                    <div className="card">
-                        <input 
-                        type="password" 
-                        className="form-control" 
-                        placeholder="Password" 
-                        required="required"
-                        />
-                    </div>
-                    <div className="card">
-                        <button type="submit" className="login-btn">Log in </button>
-                    </div>       
-                </form>
-                <p className="text-center"><a href="/signup">Create an Account</a></p>
-            </div>
-    );
-    }
+   state ={
+       username: '',
+       password: ''
+   }   
+ 
+   submitButtonhandler = e =>{
+       e.preventDefault();
+      
+   }
+ 
+  
+   render(){
+       return(
+           <div className="logincard">
+               <form action="/examples/actions/confirmation.php" method="post">
+                   <h2 className="text-center">Log in</h2>      
+                   <div className="card">
+                       <input
+                       type="text"
+                       className="form-control"
+                       placeholder="Username"
+                       required="required"
+                       />
+                   </div>
+                   <div className="card">
+                       <input
+                       type="password"
+                       className="form-control"
+                       placeholder="Password"
+                       required="required"
+                       />
+                   </div>
+                   <div className="card">
+                       <button type="submit" className="login-btn">Log in </button>
+                   </div>      
+               </form>
+               <p className="text-center"><a href="/signup">Create an Account</a></p>
+           </div>
+   );
+   }
 }
-
+ 
 export default Login;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,96 +1,96 @@
 import React, {Component } from "react";
 import './signup.css';
 import API from '../utils/API'
-
+ 
 class Signup extends Component{
-    state = {
-        username: '',
-        email: '',
-        password: ''
-    };
-
-    userSave = () => {
-        API.createUser({
-            email: this.state.email,
-            password: this.state.password
-        })
-        .then(res => {
-            console.log(res)
-            alert('Sign Up successful!');
-            window.location = "/elevate";
-        })
-        .catch(err => {
-            alert(err);
-        })
-    }
-
-    handleTyping = e => {
-        const target = e.target;
-        let value = target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    handleFormSubmit = e => {
-        e.preventDefault()
-        if (!this.state.email || !this.state.password || !this.state.email){
-            alert('One or more form fields missing inputs');
-        }else{
-            this.userSave();
-        }
-
-        this.setState({
-            username: '',
-            email: '',
-            password: ''
-        });
-    }
-
-    render(){
-        return (            
-            <div>
-                <form>
-                    <h2 className="text-center">Sign Up</h2>       
-                    <div className="card">
-                        <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="Username" 
-                        onChange={this.handleTyping}
-                        name = "username"
-                        value={this.state.username}
-                        />
-                    </div>
-                    <div className="card">
-                        <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="Email" 
-                        onChange={this.handleTyping}
-                        name = "email"
-                        value={this.state.email}
-                        />
-                    </div>
-                    <div className="card">
-                        <input 
-                        type="password" 
-                        className="form-control"
-                        placeholder="Password" 
-                        onChange={this.handleTyping}
-                        name = "password"
-                        value={this.state.password}/>
-                    </div>
-                    <div className="card">
-                        <button type="submit" className="signup-btn" onClick={this.handleFormSubmit}>Sign Up </button>
-                    </div>       
-                </form>
-                <p className="text-center"><a href="/login">Already have an account? Sign in!</a></p>
-            </div>
-        );
-    }
+   state = {
+       username: '',
+       email: '',
+       password: ''
+   };
+ 
+   userSave = () => {
+       API.createUser({
+           email: this.state.email,
+           password: this.state.password
+       })
+       .then(res => {
+           console.log(res)
+           alert('Sign Up successful!');
+           window.location = "/elevate";
+       })
+       .catch(err => {
+           alert(err);
+       })
+   }
+ 
+   handleTyping = e => {
+       const target = e.target;
+       let value = target.value;
+       const name = target.name;
+ 
+       this.setState({
+           [name]: value
+       });
+   }
+ 
+   handleFormSubmit = e => {
+       e.preventDefault()
+       if (!this.state.email || !this.state.password || !this.state.email){
+           alert('One or more form fields missing inputs');
+       }else{
+           this.userSave();
+       }
+ 
+       this.setState({
+           username: '',
+           email: '',
+           password: ''
+       });
+   }
+ 
+   render(){
+       return (           
+           <div>
+               <form>
+                   <h2 className="text-center">Sign Up</h2>      
+                   <div className="card">
+                       <input
+                       type="text"
+                       className="form-control"
+                       placeholder="Username"
+                       onChange={this.handleTyping}
+                       name = "username"
+                       value={this.state.username}
+                       />
+                   </div>
+                   <div className="card">
+                       <input
+                       type="text"
+                       className="form-control"
+                       placeholder="Email"
+                       onChange={this.handleTyping}
+                       name = "email"
+                       value={this.state.email}
+                       />
+                   </div>
+                   <div className="card">
+                       <input
+                       type="password"
+                       className="form-control"
+                       placeholder="Password"
+                       onChange={this.handleTyping}
+                       name = "password"
+                       value={this.state.password}/>
+                   </div>
+                   <div className="card">
+                       <button type="submit" className="signup-btn" onClick={this.handleFormSubmit}>Sign Up </button>
+                   </div>      
+               </form>
+               <p className="text-center"><a href="/login">Already have an account? Sign in!</a></p>
+           </div>
+       );
+   }
 }
-
+ 
 export default Signup;
