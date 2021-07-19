f8b36f473156acf95604a290db109b82e7816f51
Tim O'Malley <tomalley24@gmail.com>
2021-07-19 12:45:16 -0500

    map properly loads stations and sation data

--- client/src/pages/Elevate.js
+++ client/src/pages/Elevate.js
@@ -1,25 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
+import API from "../utils/API";
 import Map from '../components/map';
 
 
 
-function Elevate() {
+class Elevate extends Component {
     //load stations taht are saved on card components
     //load in map component
     //nav buttons or something
+    state = {
+        nearbyStations: []
+    }
+
+    // componentDidMount(){
+    //     API.getStations()
+    //     .then(res => this.setState({nearbyStations: res.data}))
+    // }
 
 
-    return (
-        <div style= {{width: '50vw', height: '50vh'}}>
-         <Map 
-            googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAZx2oaWVFfDoJOeaZxiE8wqY9RxWE4Mko`}
-            loadingElement = {<div style={{height: "100%"}} />}
-            containerElement = {<div style={{height: "100%"}} />}
-            mapElement = {<div style={{height: "100%"}} />}
-         />
-        </div>
+    render(){
+        return (
+            <div>
+                <div style= {{width: '50vw', height: '50vh'}}>
+                    <Map 
+                        googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAZx2oaWVFfDoJOeaZxiE8wqY9RxWE4Mko`}
+                        loadingElement = {<div style={{height: "100%"}} />}
+                        containerElement = {<div style={{height: "100%"}} />}
+                        mapElement = {<div style={{height: "100%"}} />}
+                    />
+                </div>
+            </div>
+    
 
-    );
+        );
+    }
   }
 
 
